5dd278b308b968219f13d744f74c113a8b56e0da
ku4umid3e <kuchumid3e@gmail.com>
2022-04-27 19:19:54 +0300

    added details for blog

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,9 @@
+from django import views
 from django.urls import path, include
 
-from .views import all_blogs
+from .views import all_blogs, detail
 
 urlpatterns = [
     path('', all_blogs, name='all_blogs'),
+    path('<int:blog_id>/', detail, name='detail')
 ]
