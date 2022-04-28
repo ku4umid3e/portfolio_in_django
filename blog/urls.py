fde72d4fe8d5fa1a50890223fd346da44035077a
ku4umid3e <kuchumid3e@gmail.com>
2022-04-28 11:02:40 +0300

    Added routing between home page and blog page

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,13 @@
 from django import views
 from django.urls import path, include
 
-from .views import all_blogs, detail
+from . import views
+
+
+app_name = 'blog'
+
 
 urlpatterns = [
-    path('', all_blogs, name='all_blogs'),
-    path('<int:blog_id>/', detail, name='detail')
+    path('', views.all_blogs, name='all_blogs'),
+    path('<int:blog_id>/', views.detail, name='detail')
 ]
