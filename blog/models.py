67d1d916cc114f4da2edd70a0f6e582fcba18a30
ku4umid3e <kuchumid3e@gmail.com>
2022-04-29 11:44:32 +0300

    Added bootstrap and base template

--- blog/models.py
+++ blog/models.py
@@ -3,5 +3,8 @@ from django.db import models
 
 class Blog(models.Model):
     title = models.CharField(max_length=100)
-    description = models.CharField(max_length=250)
+    description = models.TextField(max_length=1000)
     date = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
